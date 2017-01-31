3a1d1fcf600c322ed100290aaf4c9230b017a966
solproject1 <CEPE-S1-13@ensae.fr>
2017-01-31 12:42:01 +0100

    representation basique des stations

--- ScriptsR/bacasable/exploration_primaire_des_donnees.R
+++ ScriptsR/bacasable/exploration_primaire_des_donnees.R
@@ -1,4 +1,7 @@
 library(jsonlite)
+library(curl)
+library(cluster)
+library(RgoogleMaps)
 
 # taille des objets en memoire : 
 # sapply(ls(),function(x) format(object.size(get(x)),'auto'))
@@ -18,11 +21,21 @@ stations$lat <- stations$position$lat
 stations$lon <- stations$position$lng
 
 sapply(data_frame,class); head(data_frame)
+sapply(stations,class); head(stations)
 
+# fusion des bases 
 coordstations <- merge(data_frame, stations[,c('number','lat','lon')], by="number", all.x=T)
-coordstations <- coordstations[,c(1,9,10)]
-#on n'a pas les coordonn?es de certaines stations donc on va les enlever pour l'analyse (surtout l'analyse g?ographique)
-lignemanquantecoord<-which(is.na(coordstations[,2]))
-#coordstations<-coordstations[-lignemanquantecoord,]
-coordstations[lignemanquantecoord,c(2)]<-mean(coordstations[,2], na.rm = T)
-coordstations[lignemanquantecoord,c(3)]<-mean(coordstations[,3], na.rm = T)
+# stations_sans_coords <- unique(coordstations[,c(1,9,10)])
+# stations_sans_coords <- test[is.na(stations_sans_coords$lat) | is.na(stations_sans_coords$lon),]
+
+# retrait des stations sans coordonnées de la base 
+lignemanquantecoord <- which(is.na(coordstations[,'lat']) | is.na(coordstations[,'lon']))
+coordstations <- coordstations[-lignemanquantecoord,]
+
+# representation des station sur la carte de paris
+representation_basique <- unique(coordstations[,c('lat','lon')])
+center<-c(mean(range(representation_basique[,'lat'])),mean(range(representation_basique[,'lon'])))
+zoom<-MaxZoom(range(representation_basique[,'lat'])*1.1,range(representation_basique[,'lon'])*1.1 )
+carte<-GetMap(center=center, zoom=zoom)
+PlotOnStaticMap(carte, lat=representation_basique[,'lat'], lon=representation_basique[,'lon'], pch=16, cex=1, col='blue')
+
