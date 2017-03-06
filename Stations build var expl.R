f2a4b68fe97e834870e1a775b43f3358665cb401
denismolin <CEPE-S1-12@ensae.fr>
2017-03-06 10:35:17 +0100

    correction avec nouveau fichier RATP

--- Stations build var expl.R
+++ Stations build var expl.R
@@ -7,7 +7,7 @@ library(jsonlite)
 library(curl)
 library(cluster)
 
-#pour avoir les altitudes et la cl? d'API associ?e
+#pour avoir les altitudes et la clé d'API associée
 library(googleway)
 api_key <- "AIzaSyDIJyuMBFCW-BAMZYfjmITgDB07j1tCDDY"
 
@@ -16,15 +16,15 @@ require(geosphere)
 library(ggmap)
 library(RgoogleMaps)
 
-#r?cup?ration de la liste officielle des stations
+#récupération de la liste officielle des stations
 #################################################
 
 url="http://vlsstats.ifsttar.fr/data/input_Paris.json"
 stations<-fromJSON(sprintf("[%s]", paste(readLines(url), collapse=",")))
 stations<-stations[[1]]
 
-#r?cup?ration de la latitude et de la longitude 
-# et cr?ation comme une v?ritable variable
+#récupération de la latitude et de la longitude 
+# et création comme une véritable variable
 #(initialement dans un vecteur position)
 stations$lat<-stations$position$lat
 stations$lon<-stations$position$lng
@@ -32,20 +32,20 @@ stations$lon<-stations$position$lng
 
 
 
-#r?cup?ration de la hauteur des stations
+#récupération de la hauteur des stations
 #########################################
 
 #vecteur temporaire des altitudes des stations
 alt=rep(0,nrow(stations))
 nstations200<-floor(nrow(stations)/200)
 i<-0
-#on fait une boucle car le programme e g?re pas toue la liste des stations d'un coup
+#on fait une boucle car le programme e gère pas toue la liste des stations d'un coup
 for (i in 0:(nstations200-1))
 {
   df_locations <- stations[(i*200+1):((i+1)*200),c(13,14)]
   alt[(i*200+1):((i+1)*200)]<-google_elevation(df_locations = df_locations, key = api_key)$results[,1]
 }
-# on compl?te par les statins restantes
+# on complète par les statins restantes
 df_locations <- stations[(nstations200*200+1):(nrow(stations)),c(13,14)]
 alt[(nstations200*200+1):(nrow(stations))]<-google_elevation(df_locations = df_locations, key = api_key)$results[,1]
 #on affecte au dataframe station et on nettoie
@@ -61,9 +61,9 @@ rm(nstations200)
 
 #Fonction voisindist(dist)
 #argument : rayon 'dist' autour des stations  dans lequel on regarde (en m)
-#sortie : une liste de vecteurs de num?ro de stations (num?ros dans l'ordre de la matrice station)
-# (pour la station i, listedist[[i]] est un vecteur des stations proches de la station de la i?me ligne de la matrice stations)
-#la matrice station est consid?r?e comme une variable globale
+#sortie : une liste de vecteurs de numéro de stations (numéros dans l'ordre de la matrice station)
+# (pour la station i, listedist[[i]] est un vecteur des stations proches de la station de la ième ligne de la matrice stations)
+#la matrice station est considérée comme une variable globale
 voisindist <- function(dist)
 {
   listedist<-vector('list', nrow(stations))
@@ -79,10 +79,10 @@ voisindist <- function(dist)
 #################################################################
 
 #Fonction voisinnum(n)
-#argument : nombre n de stations ? retenir
+#argument : nombre n de stations à retenir
 #sortie : une matrice de nrow(stations) lignes et n colonnes
-# la premi?re colonne donne la station la plus proche parmi les n et la n est la plus ?loign?e
-#la matrice station est consid?r?e comme une variable globale
+# la première colonne donne la station la plus proche parmi les n et la n est la plus éloignée
+#la matrice station est considérée comme une variable globale
 voisinnum <- function(n)
 {
   out<-matrix(0,nrow=nrow(stations), ncol=n )
@@ -100,7 +100,7 @@ voisinnum <- function(n)
 #######################
 
 # Enrichissement de stations avec le nombre de bike_stands dans un rayon de x m
-# (+ ou - notion de densit? de bike_stand)
+# (+ ou - notion de densité de bike_stand)
 # Essai avec 3 variables 200m, 500m, et 1000m
 
 liste<-voisindist(200)
@@ -163,19 +163,17 @@ for (i  in (1:nrow(stations)))
   stations$hr10[i]<-stations[i,"alt"]-sum(stations[matrixvoisin[i,],"alt"])/5
 }
 
+rm(matrixvoisin)
 
 
-
-# Distance ? la station de m?tro la plus proche
+# Distance à la station de métro la plus proche
 #################################################
 
-#r?cup?ration des donn?es RATP stock?es dans le fichier coordRATP.json
+#récupération des données RATP stockées dans le fichier coordRATP.json
 #a priori, on se sert pour l'instant seulement des latitudes et longiudes des stations
 url="data/coordRATP.json"
 RATP<-fromJSON(sprintf("[%s]", paste(readLines(url), collapse=",")))
-RATPlonlat<-cbind(RATP$elements$lat,RATP$elements$lon)
-RATPna<-is.na(RATPlonlat[,1])*is.na(RATPlonlat[,2])
-RATPlonlat<-RATPlonlat[RATPna==0,]
+RATPlonlat<-as.data.frame(matrix((unlist(RATP[[1]]$fields$coord)), ncol=2, byrow = T))
 
 #visualisation des stations 
 center<-c(mean(range(RATPlonlat[,1])),mean(range(RATPlonlat[,2])))
@@ -183,15 +181,17 @@ zoom<-MaxZoom(range(RATPlonlat[,1])*1.1,range(RATPlonlat[,2])*1.1 )
 carte<-GetMap(center=center, zoom=10)
 PlotOnStaticMap(carte, lat=RATPlonlat[,1], lon=RATPlonlat[,2], pch=16, cex=1, col="red")
 
-#cr?ation de la variable $RATP
-#distance ? la sation de m?tro/RER/tram la plus proche
+#création de la variable $RATP
+#distance à la sation de métro/RER/tram la plus proche
 for (i  in (1:nrow(stations)))
 {
   stations$RATP[i]<-min(distGeo(stations[i,13:14], RATPlonlat ))
 }
 
+rm(RATP)
+rm(RATPlonlat)
 
-# R?cup?ration de la base de donn?e ?quipement
+# Récupération de la base de donnée équipement
 #################################################
 
 equip<-read.dbf(file = "data/bpe_ensemble_xy.dbf")
@@ -202,36 +202,36 @@ equip$nacoord <- 1
 equip[(is.na(equip$lambert_x)==T),"nacoord"] <- 0
 
 table(equip$typequ, equip$nacoord)
-# r?sultat : il n'y a pas tout. En services et commerces, ce n'est pas trop grave car 
-# ce ne sont pas les variables a priori les + pertinentes (? v?rifier ex post)
-# sinon quelques ?quipements autres non g?olocalis?s mais en faible proportion
-# sauf les cin?mas qui sont donc la variable a priori la plus manquante
-# les bases des ann?es pr?c?dentes n'apportent pas plus d'info
+# résultat : il n'y a pas tout. En services et commerces, ce n'est pas trop grave car 
+# ce ne sont pas les variables a priori les + pertinentes (à vérifier ex post)
+# sinon quelques équipements autres non géolocalisés mais en faible proportion
+# sauf les cinémas qui sont donc la variable a priori la plus manquante
+# les bases des années précédentes n'apportent pas plus d'info
 
-#enrichissement de la base par des cat?gories plus grosses choisies ? la main
+#enrichissement de la base par des catégories plus grosses choisies à la main
 equip$typequ2<-""
 equip[substring(equip$typequ,1,1)== "A","typequ2"]<-"A" #services
 equip[substring(equip$typequ,1,1)== "B","typequ2"]<-"B" #commerces
 equip[substring(equip$typequ,1,2)== "B1","typequ2"]<-"B1" #grandes surfaces
-equip[substring(equip$typequ,1,2)== "C1","typequ2"]<-"C1" # ?coles maternelles et ?l?mentaires
-equip[substring(equip$typequ,1,2)== "C2","typequ2"]<-"C2" # coll?ge
-equip[substring(equip$typequ,1,2)== "C3","typequ2"]<-"C3" # Lyc?es
+equip[substring(equip$typequ,1,2)== "C1","typequ2"]<-"C1" # écoles maternelles et élémentaires
+equip[substring(equip$typequ,1,2)== "C2","typequ2"]<-"C2" # collège
+equip[substring(equip$typequ,1,2)== "C3","typequ2"]<-"C3" # Lycées
 equip[substring(equip$typequ,1,2)== "C4","typequ2"]<-"C4" # Formations post bac non universitaire
-equip[substring(equip$typequ,1,2)== "C5","typequ2"]<-"C5" # Enseignement sup?rieur
+equip[substring(equip$typequ,1,2)== "C5","typequ2"]<-"C5" # Enseignement supérieur
 equip[substring(equip$typequ,1,2)== "C6","typequ2"]<-"C6" # Autres formations
-equip[substring(equip$typequ,1,2)== "C7","typequ2"]<-"C7" # r?sidence et restau U
+equip[substring(equip$typequ,1,2)== "C7","typequ2"]<-"C7" # résidence et restau U
 equip[substring(equip$typequ,1,1)== "D","typequ2"]<-"D" # Soin et social
-equip[substring(equip$typequ,1,2)== "D1","typequ2"]<-"D1" # Hopitaux et assimil?s
+equip[substring(equip$typequ,1,2)== "D1","typequ2"]<-"D1" # Hopitaux et assimilés
 equip[substring(equip$typequ,1,2)== "E1","typequ2"]<-"E1" # Taxis
-equip[substring(equip$typequ,1,2)== "E2","typequ2"]<-"E2" # A?roports
+equip[substring(equip$typequ,1,2)== "E2","typequ2"]<-"E2" # Aéroports
 equip[substring(equip$typequ,1,2)== "E3","typequ2"]<-"E3" # Gares
 equip[substring(equip$typequ,1,2)== "F1","typequ2"]<-"F1" # Sport
 equip[substring(equip$typequ,1,2)== "F2","typequ2"]<-"F2" # Nautique
-equip[substring(equip$typequ,1,2)== "F3","typequ2"]<-"F3" # Culture (Cin? th?atre mus?es)
+equip[substring(equip$typequ,1,2)== "F3","typequ2"]<-"F3" # Culture (Ciné théatre musées)
 equip[((substring(equip$typequ,1,4)== "G103") | (substring(equip$typequ,1,4)== "G104")),"typequ2"]<-"Ga" # Hotels et campings
 equip[((substring(equip$typequ,1,4)== "G101") | (substring(equip$typequ,1,4)== "G102")),"typequ2"]<-"Gb" # Equipement pour tourisme
 
-#fonction qui convertit les coordonn?es lambert 93  en latitude longitude
+#fonction qui convertit les coordonnées lambert 93  en latitude longitude
 lambert93ToWgs84 <- function(x, y)
 {
   b6  <- 6378137.0000
@@ -281,9 +281,9 @@ PlotOnStaticMap(carte, lat=equip[((equip$typequ2==type)&(!is.na(equip$lat))),13]
 typeequip<-levels(equip$typequ2)
 n_equip <- length(typeequip)
 
-#fonction qui donne la distance minimale entre une coordonn?es et un ?quipement de type "type"
+#fonction qui donne la distance minimale entre une coordonnées et un équipement de type "type"
 #@param coord : un vecteur de lat/lon,
-#type un type d'?quipement correspondant ? la colonne typequ2 de la matrice equip (ex "E2")
+#type un type d'équipement correspondant à la colonne typequ2 de la matrice equip (ex "E2")
 #@res : un nombre : la distance minimale
 distanceequip <- function(coord,type)
 {
