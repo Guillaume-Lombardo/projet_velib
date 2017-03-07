233079edb100a29ba3bb4df583f64d2c3a45ad20
Guillaume-Lombardo <lombardo.guillaume@gmail.com>
2017-03-07 03:34:23 +0100

    representations diverses des cluster et des classes.

--- clustering_kmeans.R
+++ clustering_kmeans.R
@@ -7,18 +7,17 @@ representation_kmeans <- lapply(1:10,function(.x) merge(data.frame(number = stat
 																																	 station_x_date_mod7j[,-1]), 
 																												stations_gps, by="number", all.x=F))
 
-### representation graphique des kmeans en fonction du nombre de groupe
+### representation graphique des kmeans en fonction du nombre de groupe (stocker dans map_plot_i_cluster)
 # tracer le graphique grid a : grid.draw(a)
 for(i in 2:10){
 	ifelse(is.null(dev.list()),1,dev.off()) # nettoye l'ecran pour enregistrer la carte avec les groupes 
 	PlotOnStaticMap(carte, lat=representation_kmeans[[i]][,'lat'], lon=representation_kmeans[[i]][,'lon'], 
 									pch=16, cex=1, 
-									col=brewer.pal(i, 'Paired')[as.numeric(representation_kmeans[[i]]$cluster)])
+									col=brewer.pal(10, 'Paired')[as.numeric(representation_kmeans[[i]]$cluster)])
 	grid.echo()
 	eval(parse(text = paste('map_plot_',i,'_cluster <- grid.grab()',sep = '')))
 	dev.off()
 }
-
 
 # grid.draw(map_plot_2_cluster)
 # grid.draw(map_plot_3_cluster)
@@ -29,3 +28,78 @@ for(i in 2:10){
 # grid.draw(map_plot_8_cluster)
 # grid.draw(map_plot_9_cluster)
 # grid.draw(map_plot_10_cluster)
+
+### profil par classe
+
+moyenne_par_classe <- function(x){
+	liste_variable_temps <- names(representation_kmeans[[x]])[which(substr(names(representation_kmeans[[x]]),1,1)=='T')]
+	liste_moyenne <- paste('M', substring(liste_variable_temps,2)," = mean(",liste_variable_temps,",na.rm=T)",sep = '') %>%
+		paste(collapse = ', ')
+	chaine_dplyr <-  paste('res <- representation_kmeans[[',x,']] %>% group_by(cluster) %>% summarise(',liste_moyenne,')',sep = '')
+	eval(parse(text =chaine_dplyr))
+	return(res)
+}
+
+profil_par_classe <- lapply(1:10,moyenne_par_classe)
+
+# tracer profil de classe en fonction du nombre de classes
+for(i in 2:10){
+	print(i)
+	ifelse(is.null(dev.list()),1,dev.off())
+	couleur <- brewer.pal(10, 'Paired')
+	n <- ncol(profil_par_classe[[i]])
+	xx <- seq_along(names(profil_par_classe[[i]])[-1])
+	plot(xx,as.data.frame(profil_par_classe[[i]][1,2:n]),type = 'l',
+			 col = couleur[1],ylim = c(0,1),
+			 ylab = 'proportion de velib dans la classe',
+			 xlab = '')
+	for(j in 2:i){
+		lines(xx,as.data.frame(profil_par_classe[[i]][j,2:n]),type = 'l',
+					col = couleur[j])  
+	}
+	legend(450,1,legend = 1:i,col = couleur[1:i],pch = 15)
+	grid.echo()
+	eval(parse(text = paste('profil_de_classe_',i,'_cluster <- grid.grab()',sep = '')))
+	dev.off()
+}
+
+# grid.draw(profil_de_classe_2_cluster)
+# grid.draw(profil_de_classe_3_cluster)
+# grid.draw(profil_de_classe_4_cluster)
+# grid.draw(profil_de_classe_5_cluster)
+# grid.draw(profil_de_classe_6_cluster)
+# grid.draw(profil_de_classe_7_cluster)
+# grid.draw(profil_de_classe_8_cluster)
+# grid.draw(profil_de_classe_9_cluster)
+# grid.draw(profil_de_classe_10_cluster)
+
+# tracer profil de classe avec les membre de la classe
+l <- 5L
+tracer_profil_membres <- function(nb_classes, classe, nb_membre){
+	ifelse(is.null(dev.list()),1,dev.off())
+	couleur <- brewer.pal(10, 'Paired')
+	n <- ncol(representation_kmeans[[nb_classes]])-3
+	plot(xx,as.data.frame(profil_par_classe[[nb_classes]][classe,2:n]),type = 'l',col = couleur[classe],ylim = c(0,1),
+			 ylab = 'proportion de velib dans la classe',
+			 xlab = '')
+	for(j in sample(which(representation_kmeans[[nb_classes]]$cluster==classe),nb_membre)){
+			lines(xx,representation_kmeans[[nb_classes]][j,2:n + 1], col = 'lightgray')	
+	}
+	lines(xx,as.data.frame(profil_par_classe[[nb_classes]][classe,2:n]), col = couleur[k])
+	grid.echo()
+	eval(parse(text = paste('profil_',nb_classes,'_cluster_classe_',classe,' <- grid.grab()',sep = '')))
+}
+
+for(i in 2:10){
+	for (k in 1:i){
+		tracer_profil_membres(i,k,l)
+		dev.off()
+	}
+}
+
+# grid.draw(profil_6_cluster_classe_1)
+# grid.draw(profil_6_cluster_classe_2)
+# grid.draw(profil_6_cluster_classe_3)
+# grid.draw(profil_6_cluster_classe_4)
+# grid.draw(profil_6_cluster_classe_5)
+# grid.draw(profil_6_cluster_classe_6)
