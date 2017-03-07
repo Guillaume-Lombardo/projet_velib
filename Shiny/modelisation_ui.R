d6b36295835c7e51e8ae00097c7452125f412a2e
denismolin <CEPE-S1-12@ensae.fr>
2017-03-07 10:26:02 +0100

    modelisationui

--- Shiny/modelisation_ui.R
+++ Shiny/modelisation_ui.R
@@ -1,3 +1,16 @@
 
+# ouvrir une ligne horizontale
+fluidRow(
+  # premiere colonne pour les entrees utilisateurs
+  column(width = 2, 
+         wellPanel(
+           numericInput("bins", "Number of bins:", min = 1, max = 50, value = 30)
+  )),
+  # deuxieme colonne avec les sortiesw
+  column(width = 8, 
+         
+         textOutput("mod1")
+         
+  )
+  )
 
-textOutput("mod1")
