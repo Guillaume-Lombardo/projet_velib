ce0edcf7d3b20a37ceea8184590e59e559b670c1
CEPE-S1-14 <CEPE-S1-14@ensae.fr>
2017-03-07 14:55:11 +0100

    maj expolration_ui

--- Shiny/exploration_ui.R
+++ Shiny/exploration_ui.R
@@ -1,11 +1,12 @@
 
 fluidRow(
-  column(2,selectInput("B_var1","Choix de variable 1 :",liste_variables_exp[,2]),
+  column(2,
+         selectInput("B_var1","Choix de variable 1 :",liste_variables_exp[,2]),
          checkboxInput("B_var1_norm","Centrer-réduire Var1?"),
          checkboxInput("B_bivarie", "Bivarié?", value = FALSE),
          conditionalPanel(
             condition = "input.B_bivarie == true",
-            selectInput("B_var2", "Choix de variable 2 :", c(2:10)),
+            selectInput("B_var2", "Choix de variable 2 :",liste_variables_exp[,2]),
             checkboxInput("B_var2_norm","Centrer-réduire Var2?")),
          actionButton("B_go","Mettre à jour")
          ),
