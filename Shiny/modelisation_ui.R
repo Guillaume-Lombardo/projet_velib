ba9aeeecb220bbfe1b0d0c2403c6ef5a22b35133
denismolin <CEPE-S1-12@ensae.fr>
2017-03-07 10:40:57 +0100

    mod3

--- Shiny/modelisation_ui.R
+++ Shiny/modelisation_ui.R
@@ -4,9 +4,18 @@ fluidRow(
   # premiere colonne pour les entrees utilisateurs
   column(width = 2, 
          wellPanel(
+           
+           #selection du nombre de classes
+           selectInput(inputId = "Ckmeans", label = "Nombre de classes", selected = 6,
+                       choices = 2:10), 
+           
+           #selection du modèle
            selectInput(inputId = "Cselecmod", label = "Choix du modèle", selected = 1,
                        choices = c("Lasso" = 1, "Ridge" = 2, "Elasticnet" = 3,
-                                   "Random Forest" = 4, "SVM"=5))
+                                   "Random Forest" = 4, "SVM"=5)),
+           
+           #Proposition de centrer-réduire les variables
+           checkboxInput(inputId = "Cscale", label = "Centrer-réduire ?")
   )),
   # deuxieme colonne avec les sortiesw
   column(width = 8, 
