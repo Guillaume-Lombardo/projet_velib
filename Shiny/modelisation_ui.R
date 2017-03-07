b2c6ab1f8834f4a2825a105bf3cb4e3954cd24be
denismolin <CEPE-S1-12@ensae.fr>
2017-03-07 10:31:22 +0100

    mod2

--- Shiny/modelisation_ui.R
+++ Shiny/modelisation_ui.R
@@ -4,7 +4,9 @@ fluidRow(
   # premiere colonne pour les entrees utilisateurs
   column(width = 2, 
          wellPanel(
-           numericInput("bins", "Number of bins:", min = 1, max = 50, value = 30)
+           selectInput(inputId = "Cselecmod", label = "Choix du modèle", selected = 1,
+                       choices = c("Lasso" = 1, "Ridge" = 2, "Elasticnet" = 3,
+                                   "Random Forest" = 4, "SVM"=5))
   )),
   # deuxieme colonne avec les sortiesw
   column(width = 8, 
