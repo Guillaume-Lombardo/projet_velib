5d1bd427f4c790929b8060506192219ba3badbfa
denismolin <CEPE-S1-12@ensae.fr>
2017-03-20 16:07:55 +0100

    comp modele server

--- Shiny/Comp_modeles_server.R
+++ Shiny/Comp_modeles_server.R
@@ -21,5 +21,41 @@ output$Dtablecompmodele <- renderDataTable({
   }
   url<-paste0(url,"Biensclasses.RDS")
   table <- readRDS(file = url)
-  datatable(table, options = list(pageLength = 12))
+  datatable(table, options = list(pageLength = 12))%>%
+    formatStyle(
+      'clusters 2',
+      color = styleInterval(c(max(table[,1])-0.0001), c('black', 'red'))
+    )%>%
+  formatStyle(
+    'clusters 3',
+    color = styleInterval(c(max(table[,2])-0.0001), c('black', 'red'))
+  )%>%
+  formatStyle(
+    'clusters 4',
+    color = styleInterval(c(max(table[,3])-0.0001), c('black', 'red'))
+  )%>%
+  formatStyle(
+    'clusters 5',
+    color = styleInterval(c(max(table[,4])-0.0001), c('black', 'red'))
+  )%>%
+  formatStyle(
+    'clusters 6',
+    color = styleInterval(c(max(table[,5])-0.0001), c('black', 'red'))
+  )%>%
+  formatStyle(
+    'clusters 7',
+    color = styleInterval(c(max(table[,6])-0.0001), c('black', 'red'))
+  )%>%
+  formatStyle(
+    'clusters 8',
+    color = styleInterval(c(max(table[,7])-0.0001), c('black', 'red'))
+  )%>%
+  formatStyle(
+    'clusters 9',
+    color = styleInterval(c(max(table[,8])-0.0001), c('black', 'red'))
+  )%>%
+  formatStyle(
+    'clusters 10',
+    color = styleInterval(c(max(table[,9])-0.0001), c('black', 'red'))
+  )
 })
